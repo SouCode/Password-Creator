115c584d147fc5ca4882a7ac82e16f6bcc9ab22b
Sou <samelsonjohn@protonmail.com>
2023-06-23 14:51:30 -0400

    added comments

--- src/Main.java
+++ src/Main.java
@@ -2,37 +2,36 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        // Press Opt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
         System.out.println("Enter password length: ");
         Scanner input = new Scanner(System.in);
 
-        int digits = input.nextInt(); // How long the password should be generted to
-        String lowerCase = "qwertyuiopasdfghjklzxcvbnm";
-        String upperCase = "QWERTYUUIOPASDFGHJKLZXCVBNM";
-        String password = "";
+        int digits = input.nextInt(); // How long the password should be generated
 
-        for(int i = 0; i < digits; i++) {
-            int rand = (int)(3 * Math.random());
+        String lowerCase = "qwertyuiopasdfghjklzxcvbnm"; // Lowercase letters
+        String upperCase = "QWERTYUUIOPASDFGHJKLZXCVBNM"; // Uppercase letters
+        String password = ""; // Variable to store the generated password
 
-            switch(rand) {
+        for (int i = 0; i < digits; i++) {
+            int rand = (int) (3 * Math.random()); // Generate a random number between 0 and 2
+
+            switch (rand) {
                 case 0:
-                    password += String.valueOf((int)(0 * Math.random()));
+                    // If rand is 0, append a random digit (0-9) to the password
+                    password += String.valueOf((int) (0 * Math.random()));
                     break;
                 case 1:
-                    rand = (int)(lowerCase.length() * Math.random());
+                    // If rand is 1, append a random lowercase letter to the password
+                    rand = (int) (lowerCase.length() * Math.random());
                     password += String.valueOf(lowerCase.charAt(rand));
                     break;
 
                 case 2:
-                    rand = (int)(upperCase.length() * Math.random());
+                    // If rand is 2, append a random uppercase letter to the password
+                    rand = (int) (upperCase.length() * Math.random());
                     password += String.valueOf(upperCase.charAt(rand));
                     break;
             }
         }
-        System.out.println(password);
-
-
-
+        System.out.println(password); // Print the generated password
     }
 }
